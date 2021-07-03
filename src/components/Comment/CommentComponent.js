c4550e0ff99c8e3b8db5b3c60b0a9894b9843720
gingkob <malivlada@hotmail.com>
2021-07-03 13:56:46 +0200

    styled new coment component

--- src/components/Comment/CommentComponent.js
+++ src/components/Comment/CommentComponent.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
+import { IoChevronBackCircle } from 'react-icons/io5'
 import Store from '../../store/Store'
 import './NewComment.css'
 
@@ -15,14 +16,41 @@ function CommentComponent() {
     textArea.current.value = ''
     history.replace('/my-readings')
   }
+
+  const handleClickBack = () => {
+    history.goBack()
+  }
+
   return (
     <div className="new-comment-container">
       <div className="new-comment__heading">
         <h1>{book.title}</h1>
         <p>{book.authorName}</p>
       </div>
-      <textarea ref={textArea} rows={8} cols={55} />
-      <button onClick={handleClick}>Save Comment</button>
+      <textarea
+        className="new-comment__textarea"
+        ref={textArea}
+        rows={8}
+        cols={50}
+        spellcheck="false"
+      />
+      <div className="new-comment__btn-container">
+        <button
+          type="button"
+          className="new-comment__btn"
+          onClick={handleClick}
+        >
+          Save Comment
+        </button>
+        <button
+          type="button"
+          className="new-comment__btn"
+          onClick={handleClickBack}
+        >
+          <IoChevronBackCircle className="new-comment__btn--icon" />
+          <span>Back</span>
+        </button>
+      </div>
     </div>
   )
 }
